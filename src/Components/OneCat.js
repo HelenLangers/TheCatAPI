ee49b1b0daa613be86ea4d654d36c90d6351ad03
Helen Langridge <langridge.helen@outlook.com>
2022-10-23 19:20:12 +0100

    css one cat block and fa icons

--- src/Components/OneCat.js
+++ src/Components/OneCat.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons'
+import { faThumbsDown } from '@fortawesome/free-regular-svg-icons'
 
-const OneCat = () => {
+const OneCat = ({cat}) => {
   return (
-    <div>OneCat</div>
+    <div className='one-cat-block'>
+            <div className='image-container'>
+                <img className='image' src={cat.url} />
+            </div>
+            <div className='overall-score'>
+                <p>Score: {cat.votes}</p>
+            </div>
+            <div className='voting-buttons-container'>
+                <button className='voting-buttons' >
+                    <FontAwesomeIcon icon={faThumbsUp} size="2x" className="thumbup" />
+                </button>
+                <button className='voting-buttons'>
+                    <FontAwesomeIcon icon={faThumbsDown} size="2x" className="thumbdown" />
+                </button>
+            </div>
+        </div>
   )
 }
 
