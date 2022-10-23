7b44b7d2b0bef0a0b7619a7774d95f432f4f60f1
Helen Langridge <langridge.helen@outlook.com>
2022-10-23 18:42:38 +0100

    changed env to config for api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import {useState, useEffect} from 'react';
 
 
 function App() {
+
+  const [twentyCats, setTwentyCats] = useState([])
+
+  useEffect(() => {
+    loadTwentyCats()
+  }, [])
+
+  const loadTwentyCats = () => {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=20&order=DESC", {
+        method: 'GET',
+        header: {"x-api-key": env.API_KEY}
+        })
+    .then(res => res.json())
+    .then((data) => {
+      const modifiedData = data.map((cat) => {
+        cat.votes = 0
+        return cat;
+      })
+      setTwentyCats(modifiedData)
+      console.log(modifiedData)
+      console.log(env.API_KEY)
+    })
+    .catch(err => console.log('Error', err));
+  }
+
   return (
     <div className="App">
 
