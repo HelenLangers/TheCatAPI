dfeafc1d0fe496e635faea8b9e72b7f8c557581c
Helen Langridge <langridge.helen@outlook.com>
2022-10-23 19:28:22 +0100

    added on click and post method for upvote

--- src/Components/OneCat.js
+++ src/Components/OneCat.js
@@ -3,9 +3,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons'
 import { faThumbsDown } from '@fortawesome/free-regular-svg-icons'
 
-const OneCat = ({cat}) => {
-  return (
-    <div className='one-cat-block'>
+const OneCat = ({ cat }) => {
+
+    const addVote = (cat) => {
+        fetch("https://api.thecatapi.com/v1/votes", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                "x-api-key": "live_7rTfVO1Ar85eGX2Fa5CSCBHE1OZR0FmF6lE3C5jASWqiOmzwWAdO0ky9FHQEq0A3"
+            },
+            body: JSON.stringify({
+                "image_id": cat.id,
+                "sub_id": "user-1",
+                "value": 1
+            })
+        })
+            .catch(err => console.log('Error', err))
+    };
+
+    // 201 response:
+    // {
+    //     "message": "SUCCESS",
+    //     "id": 611475,
+    //     "image_id": "9c",
+    //     "sub_id": "user-1",
+    //     "value": 1,
+    //     "country_code": "GB"
+    // }
+
+    const handleVoteUpClick = () => {
+        addVote(cat);
+    }
+
+    return (
+        <div className='one-cat-block'>
             <div className='image-container'>
                 <img className='image' src={cat.url} />
             </div>
@@ -13,7 +44,7 @@ const OneCat = ({cat}) => {
                 <p>Score: {cat.votes}</p>
             </div>
             <div className='voting-buttons-container'>
-                <button className='voting-buttons' >
+                <button className='voting-buttons' onClick={handleVoteUpClick}>
                     <FontAwesomeIcon icon={faThumbsUp} size="2x" className="thumbup" />
                 </button>
                 <button className='voting-buttons'>
@@ -21,7 +52,7 @@ const OneCat = ({cat}) => {
                 </button>
             </div>
         </div>
-  )
+    )
 }
 
 export default OneCat
