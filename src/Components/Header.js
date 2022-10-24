6a75a4b81953934e148555ce8762a0955a81818f
Helen Langridge <langridge.helen@outlook.com>
2022-10-24 19:29:45 +0100

    css edit title and added subtitle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 export const Header = () => {
   return (
     <header className="header">
-    <h1 className='title'>Not another cat voting app</h1>
+    <h1 className='title'>The Cat Gallery</h1>
+    <h2 className='subtitle'>Please cats your votes...</h2>
   </header>
   )
 }
